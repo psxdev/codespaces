d21488d2a6d0e8b19bac6361f37ad8a7a10886e2
Antonio Jose Ramos Marquez <psxdev@gmail.com>
2025-08-24 13:45:01 +0200

    Update dcdev_install.sh

--- .devcontainer/dcdev_install.sh
+++ .devcontainer/dcdev_install.sh
@@ -1,7 +1,9 @@
+echo begin install dcdev
 cd /usr/local
 sudo wget https://github.com/psxdev/builder_dcdev/releases/download/latest/dcdev-ubuntu-latest.tar.gz
 sudo mkdir dcdev
 sudo chown -R codespace:codespace dcdev
 sudo chown codespace:codespace dcdev*.tar.gz
 tar xvf dcdev-ubuntu-latest.tar.gz
+echo end install dcdev
 
