23440aabaf46f43757e01f4f8cae0c83a0c0fb5c
Antonio Jose Ramos Marquez <psxdev@gmail.com>
2025-08-24 13:09:54 +0200

    Create dcdev_install.sh

---
+++ dcdev_install.sh
@@ -0,0 +1,7 @@
+cd /usr/local
+sudo wget https://github.com/psxdev/builder_dcdev/releases/download/latest/dcdev-ubuntu-latest.tar.gz
+sudo mkdir dcdev
+sudo chown -R codespace:codespace dcdev
+sudo chown codespace:codespace dcdev*.tar.gz
+tar xvf dcdev-ubuntu-latest.tar.gz
+
